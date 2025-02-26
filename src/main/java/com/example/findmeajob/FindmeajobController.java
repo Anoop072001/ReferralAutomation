819c303f4df842d51b72d8363aab89b1ca10bd95
Anoop072001 <anoopsidhan777@gmail.com>
2025-02-27 04:22:31 +0530

    Removed issues

--- src/main/java/com/example/findmeajob/FindmeajobController.java
+++ src/main/java/com/example/findmeajob/FindmeajobController.java
@@ -1,9 +1,6 @@
 package com.example.findmeajob;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 

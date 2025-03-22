c82dd50485dc76c4f1bbf40149141c397abc49ba
Anoop072001 <anoopsidhan777@gmail.com>
2025-03-23 00:21:06 +0530

    Few changes made

--- src/main/java/com/example/findmeajob/FindmeajobApplication.java
+++ src/main/java/com/example/findmeajob/FindmeajobApplication.java
@@ -2,12 +2,12 @@ package com.example.findmeajob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.findmeajob.database")
-@EntityScan("com.example.findmeajob.database")
+//@EnableJpaRepositories(basePackages = "com.example.findmeajob.database")
+//@EntityScan("com.example.findmeajob.database")
 public class FindmeajobApplication {
 
 	public static void main(String[] args) {

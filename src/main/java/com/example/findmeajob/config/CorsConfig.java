019e79397b389d4d72be430ad39d8ce080130553
Anoop072001 <anoopsidhan777@gmail.com>
2025-03-23 13:13:10 +0530

    Few changes made

--- src/main/java/com/example/findmeajob/config/CorsConfig.java
+++ src/main/java/com/example/findmeajob/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.findmeajob.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
                 .allowedOrigins("https://job-matcher-frontend.vercel.app","http://localhost:3000") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods

3929a344035b94da2ebb0e0bde49ceee2ca110b3
Anoop072001 <anoopsidhan777@gmail.com>
2025-03-23 15:47:59 +0530

    Few changes made

--- src/main/java/com/example/findmeajob/FindmeajobController.java
+++ src/main/java/com/example/findmeajob/FindmeajobController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping
-@CrossOrigin(origins = "https://job-matcher-frontend.vercel.app")
+@CrossOrigin(origins = "*")
 public class FindmeajobController {
     private final FindmeajobService resumeParserService;
     private final ResumeAnalysisService resumeAnalysisService;
--- src/main/java/com/example/findmeajob/config/CorsConfig.java
+++ src/main/java/com/example/findmeajob/config/CorsConfig.java
@@ -1,19 +1,19 @@
-package com.example.findmeajob.config;
-
-import org.jetbrains.annotations.NotNull;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(@NotNull CorsRegistry registry) {
-        registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("https://job-matcher-frontend.vercel.app","http://localhost:3000") // Replace with your frontend URL
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow cookies/auth headers if needed
-    }
-}
+//package com.example.findmeajob.config;
+//
+//import org.jetbrains.annotations.NotNull;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(@NotNull CorsRegistry registry) {
+//        registry.addMapping("/**") // Allow CORS for all endpoints
+//                .allowedOrigins("https://job-matcher-frontend.vercel.app","http://localhost:3000") // Replace with your frontend URL
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+//                .allowedHeaders("*") // Allow all headers
+//                .allowCredentials(true); // Allow cookies/auth headers if needed
+//    }
+//}

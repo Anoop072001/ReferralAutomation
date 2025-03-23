f229b6a7eadc8c5f024d14cc98ac247850448036
Anoop072001 <anoopsidhan777@gmail.com>
2025-03-23 13:12:10 +0530

    Few changes made

--- src/main/java/com/example/findmeajob/config/CorsConfig.java
+++ src/main/java/com/example/findmeajob/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("https://job-matcher-frontend.vercel.app/","http://localhost:3000") // Replace with your frontend URL
+                .allowedOrigins("https://job-matcher-frontend.vercel.app","http://localhost:3000") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow cookies/auth headers if needed
